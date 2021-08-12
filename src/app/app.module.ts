05cf9bcff4a4d48c8709dfc932690f20a5beb681
Alan Junior <afj033.aj@gmail.com>
2021-08-12 12:10:38 -0300

    Adicionando estrutura de telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AgendaComponent } from './agenda/agenda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
